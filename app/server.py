057699681ff7344f203c782882c63d0504c1ec2c
Siddharth Dangwal <maximumendurance1965@gmail.com>
2020-05-28 17:01:22 +0530

    Logistic Regression model
    Pima Indian dataset

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1SWmJxHyp04_Mrz9xH4anjvn0gy7vhHQQ'
+export_file_name = 'finalized_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Pima Indian Yes', 'Pima Indian No']
 path = Path(__file__).parent
 
 app = Starlette()
